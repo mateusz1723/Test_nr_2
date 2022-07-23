d17222db835233db772d1932be02e79096ad3b1c
mateusz.trochimowicz <trohimowicz@onet.pl>
2022-07-23 15:50:51 +0200

    Ad3ded default constructor in exception class

--- src/main/java/pl/kurs/exceptions/UjemneLiczbyWyjatek.java
+++ src/main/java/pl/kurs/exceptions/UjemneLiczbyWyjatek.java
@@ -2,6 +2,9 @@ package pl.kurs.exceptions;
 
 public class UjemneLiczbyWyjatek extends Exception {
 
+    public UjemneLiczbyWyjatek() {
+    }
+
     public UjemneLiczbyWyjatek(String message) {
         super(message);
     }
